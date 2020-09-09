32f7857d13544f0137507fbaaafbb3d81ee06d39
Odeson Honorat <44267745+sondy94@users.noreply.github.com>
2020-09-08 22:29:24 -0400

    new header new header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Select, MenuItem, FormControl } from "@material-ui/core";
+import "./header.css";
 
-function Header() {
-  return <h1>Hello World</h1>;
+function Header({ countries }) {
+  const [country, setCountry] = useState("worlwide");
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
+  return (
+    <div className="app_header">
+      <h1>COVID 19 TRACKER</h1>
+      <FormControl>
+        <Select variant="outlined" value={country} onChange={onCountryChange}>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          {countries.map((country) => (
+            <MenuItem value={country.name}>{country.name}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
 
 export default Header;
