8044fdd97f78578757245e6562a63d9e4fdb6360
Odeson Honorat <44267745+sondy94@users.noreply.github.com>
2020-09-08 22:37:15 -0400

    data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import Header from "./components/Header";
 
 export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="App">
-      <Header />
+      <Header countries={countries} />
     </div>
   );
 }
